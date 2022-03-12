79e5d0a547c437aac6e527cd1e357d3ae73a989e
patrasxd <patrasxd@gmail.com>
2022-03-12 15:30:03 +0100

    Initial commit

---
+++ TPO1_PP_S20924/src/TPO_ZAO2/Main.java
@@ -0,0 +1,19 @@
+/**
+ *
+ *  @author Popiołek Patryk S20924
+ *
+ */
+
+package TPO_ZAO2;
+
+
+public class Main {
+  public static void main(String[] args) {
+    Service s = new Service("Poland");
+    String weatherJson = s.getWeather("Warsaw");
+    Double rate1 = s.getRateFor("USD");
+    Double rate2 = s.getNBPRate();
+    // ...
+    // część uruchamiająca GUI
+  }
+}
---
+++ TPO1_PP_S20924/src/TPO_ZAO2/Service.java
@@ -0,0 +1,11 @@
+/**
+ *
+ *  @author Popiołek Patryk S20924
+ *
+ */
+
+package TPO_ZAO2;
+
+
+public class Service {
+}  
